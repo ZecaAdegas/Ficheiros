13c52bcc0809e7939b3fd8666e6ed7123b36deb9
Zeca Adegas <henrique1paulo99@gmail.com>
2022-10-03 16:03:00 +0100

    Escrever em Ficheiros

--- ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
+++ ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
@@ -1,6 +1,8 @@
 package manipulaficheiros;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 class Ficheiro {
@@ -21,9 +23,42 @@ class Ficheiro {
     }
 
     static void verificaSeExiste() {
+        String nome;
+        System.out.println("Insira o nome do ficheiro a criar: ");
+        nome = ManipulaFicheiros.ler.nextLine();
+        nome = ManipulaFicheiros.ler.next();
+        File ficheiro = new File(nome);
+        if(!ficheiro.exists()){
+                System.out.println("O ficheiro não existe");
+            }else{
+                System.out.println("O ficheiro existe");
+        }  
     }
 
     static void escreveNoFinal() {
+        String nome;
+        System.out.println("Insira o nome do ficheiro: ");
+        nome = ManipulaFicheiros.ler.nextLine();
+        nome = ManipulaFicheiros.ler.next();
+        File ficheiro = new File(nome);
+        
+        try{
+            if(!ficheiro.exists()){
+                ficheiro.createNewFile();
+            }
+            FileWriter fw = new FileWriter(ficheiro, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            String texto;
+            System.out.println("Insira o conteúdo: ");
+            texto = ManipulaFicheiros.ler.nextLine();
+            texto = ManipulaFicheiros.ler.nextLine();
+            bw.write(texto);
+            bw.newLine();
+            bw.close();
+            fw.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     static void escreveFicheiroNovo() {
