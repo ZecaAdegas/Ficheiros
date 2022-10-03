e25292de9a91da1a7888ed9a56ce1182994ede57
Zeca Adegas <henrique1paulo99@gmail.com>
2022-10-03 15:24:42 +0100

    Criar Ficheiros

--- ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
+++ ManipulaFicheiros/src/manipulaficheiros/Ficheiro.java
@@ -1,10 +1,23 @@
 package manipulaficheiros;
 
 import java.io.File;
+import java.io.IOException;
 
 class Ficheiro {
 
     static void criaFicheiro() {
+        String nome;
+        System.out.println("Insira o nome do ficheiro a criar: ");
+        nome = ManipulaFicheiros.ler.nextLine();
+        nome = ManipulaFicheiros.ler.next();
+        File ficheiro = new File(nome);
+        if(!ficheiro.exists()){
+            try {
+                ficheiro.createNewFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     static void verificaSeExiste() {
@@ -22,8 +35,8 @@ class Ficheiro {
     static void mostraListaFicheiros() {
         File ficheiro = new File("C:\\Users\\hpaulo\\Desktop\\ProjetoJava\\Ficheiros");
         File [] lista = ficheiro.listFiles();
-        for(int x = 0;x<lista.length;x++){
-            System.out.println(lista[x].getName());
+        for (File lista1 : lista) {
+            System.out.println(lista1.getName());
         }
 
         
